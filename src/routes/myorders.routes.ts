b509540956597baf53b1f7cefe74b46148144db6
isaac <isaacjeccles15@gmail.com>
2024-11-01 14:03:53 +0000

    Added admin pages and route for my orders page

---
+++ src/routes/myorders.routes.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+import Orders from '../models/order';
+import OrderItems from '../models/order-item';
+import { cache } from '../utils/middleware';
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+router.get('/', cache('dynamic'), (req: Request, res: Response) => {
+    res.render('pages/my_orders', { titleText: 'My Orders', layout: 'dashboard.hbs', req });
+});
