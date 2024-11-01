3c8acc7e79b3e89709cf3bbdf38a330cb1a715a1
Benjamin Holmes <bholmes@a-w-s.co.uk>
2024-11-01 15:34:28 +0000

    fixed typo in create order migration.

--- migrations/20241025195342-create-order-item.js
+++ migrations/20241025195342-create-order-item.js
@@ -36,6 +36,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
