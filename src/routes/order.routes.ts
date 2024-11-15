b7af53bada857ad3ec841ba5dc13416d4124060b
Benjamin Holmes <bholmes@a-w-s.co.uk>
2024-11-15 15:46:21 +0000

    Modified the route for orders page.

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,7 +2,6 @@
 
 import { validateEmail } from '../utils/validation';
 import Orders from '../models/order';
-import OrderItems from '../models/order-item';
 import Products from '../models/product';
 
 import express, { Request, Response } from 'express';
@@ -14,3 +13,23 @@ const router = express.Router();
 router.get('/', cache('dynamic'), (req: Request, res: Response) => {
     res.render('pages/orders', { titleText: 'Orders', layout: 'dashboard.hbs', req })
 });
+
+router.get('/api', (req: Request, res: Response) => {
+    Orders.findAll()
+        .then((orders) => {
+            return res.status(200).json({
+                code: 200,
+                data: {
+                    orders: orders
+                }
+            })
+        }).catch((err: Error) => {
+            console.log(err)
+            return res.status(500).json({
+                code: 500,
+                error: 'Internal server error'
+            })
+        })
+})
+
+export default router;
